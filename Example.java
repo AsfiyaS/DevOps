ad5b7414917ccedac28c9d2bcc7a30724852239b
Asfiya Sayed <sayedasfiya2001@gmail.com>
2022-10-06 10:28:59 +0530

    edited example.java

--- Example.java
+++ Example.java
@@ -1,5 +1,14 @@
 public class Example {
-    public static void main(String[] args) {
-        System.out.println("Hello Asfiya!"); 
-    }
-}
+public static void main(String[] args)   
+{  
+int i, num = 10, sum = 0;  
+//executes until the condition returns true  
+for(i = 1; i <= num; ++i)  
+{  
+//adding the value of i into sum variable  
+sum = sum + i;  
+}  
+//prints the sum   
+System.out.println("Sum of First 10 Natural Numbers is = " + sum);  
+}  
+} 
